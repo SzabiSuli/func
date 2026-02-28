97d20434bbe346e19ed9aa5fb65db1c82bad66a1
SzecsiSzabi <szabolcs.szecsi@optimaze.hu>
2026-02-28 21:11:21 +0100

    hf3 + gyakorlas

--- 2025-26-2/hf/Hf3.hs
+++ 2025-26-2/hf/Hf3.hs
@@ -18,5 +18,62 @@ data Gofri f a
 deriving instance (Eq a, Eq1 f) => Eq (Gofri f a)
 deriving instance (Show a, Show1 f) => Show (Gofri f a)
 
-instance Foldable Gofri f where
-    foldMap :: Monoid m => (a -> m) -> Tree a -> m
+instance Foldable f => Foldable (Gofri f) where
+    foldMap :: Monoid m => (a -> m) -> Gofri f a -> m
+    foldMap f (MkGofri fa x) = foldMap f fa <> foldMap (foldMap f) x
+
+
+data CrazyType3 a b
+    = CrazyCon1 a b a 
+    | CrazyCon2 (CrazyType3 a b) [b] [a]
+    | CrazyCon3 (CrazyType3 Int b) (CrazyType3 a a) [[b]]
+    deriving (Eq, Show)
+
+instance Foldable (CrazyType3 a) where
+    foldMap :: Monoid m => (b -> m) -> CrazyType3 a b -> m
+    foldMap f (CrazyCon1 x y z) = f y
+    foldMap f (CrazyCon2 x lb la) = foldMap f x <> foldMap f lb
+    foldMap f (CrazyCon3 ctib cta llb) = foldMap f ctib <> foldMap (foldMap f) llb
+
+
+
+foldMap' :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+foldMap' f = foldr ((<>) . f) mempty
+-- foldMap' f x = foldr (\a m -> f a <> m) mempty x
+
+foldr' :: (Foldable t) => (a -> b -> b) -> b -> t a -> b
+foldr' f b x = destructEndo (foldMap (Endo . f) x) b
+-- foldr' f b x = destructEndo (foldMap (\a -> Endo (f a)) x) b
+
+-- we can pass \a -> f a b
+-- if it knows mempty = b
+-- and x
+-- we need to make a monoid from b
+
+
+-- the solution is, 
+-- we can't provide a default for a generic b parameter,
+-- because b could be empty
+-- but we can make a type that 
+-- IF it recieves an element, it could carry that through with the <> operator
+
+newtype Endo b = Endo (b -> b)
+
+instance Semigroup (Endo b) where
+    (<>) :: Endo b -> Endo b -> Endo b
+    Endo f <> Endo g = Endo (f . g)
+
+instance Monoid (Endo b) where
+    mempty = Endo id
+
+destructEndo :: Endo b -> (b -> b)
+destructEndo (Endo f) = f
+
+
+foldMap'' :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+foldMap'' f = foldl (\m a -> m <> f a) mempty
+
+foldl' :: (Foldable t) => (b -> a -> b) -> b -> t a -> b
+-- foldl' f b x = destructEndo (foldMap (\a -> Endo (\y -> f y a)) x) b
+-- foldl' f b x = destructEndo (foldMap (\a -> Endo (flip f a)) x) b
+foldl' f b x = destructEndo (foldMap (Endo . flip f) x) b
