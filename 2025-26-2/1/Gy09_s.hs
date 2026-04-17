f3b39df0a07bd2669f593fc6e9728164f71b7f73
SzabiSuli <t2hlyg@inf.elte.hu>
2026-04-17 17:26:10 +0200

    gy09

---
+++ 2025-26-2/1/Gy09_s.hs
@@ -0,0 +1,218 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Gy09 where
+
+import Control.Monad
+import Control.Applicative
+import Data.Functor
+import Data.Traversable
+import Data.Foldable
+import Data.Char
+import Data.Time.Format.ISO8601 (yearFormat)
+
+-- Megaparsec
+-- Haskell industry standard hiba kezelő vagy mi
+
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) } deriving Functor
+
+-- Nagyon hasonló a State-hez
+--                        { runState  :: s      ->       (a,       s) }
+
+-- Valóságban monadTransformerekkel maybe helyett ExceptT-vel van még definiálva state mellet
+
+instance Applicative Parser where
+  pure a = Parser $ \s -> Just (a, s)
+  (<*>) = ap
+
+instance Monad Parser where
+  (Parser rP) >>= f = Parser $ \s -> case rP s of
+    Nothing -> Nothing
+    Just (a, s') -> runParser (f a) s'
+
+
+-- A Parser és a State két fő különbsége
+-- 1. A Parsernek a "belső állapota" mindig string
+-- 2. A Parser el tud hasalni (ezt a belső Maybe reprezentálja)
+-- Az elhasalásnál megjegyezhetjük miért áltunk meg egy hiba formájában
+-- Ekkor a Parsert lehetne ExceptT String (State String) a-val is reprezentálni (később ezzel is lesz)
+
+-- Primitív parserek
+-- Olyan parser, amely lenyel egy karaktert a bemenetről és akkor fogad el, ha az adott predikátum teljesül rá
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser $ \case
+  [] -> Nothing
+  (c:xs) -> if p c then Just (c, xs) else Nothing
+
+-- Olyan parser, amely akkor fogad el, ha nincs semmi a bemeneten
+eof :: Parser ()
+eof = Parser $ \case 
+  [] -> Just ((), [])
+  (_:_) -> Nothing
+
+-- dropRest :: Parser a -> Parser ()
+-- dropRest p = Parser $ do
+--   (_, xs) <- runParser p
+--   return ((), xs)
+
+-- Ezekből felépíthetőek egyéb parserek
+-- Olyan parser, ami egy adott karaktert parseol
+-- Itt irreleváns az, hogy mi a kimenet
+char :: Char -> Parser ()
+-- char c = Parser $ \inp -> satisfy (== c) inp -- nem jó
+-- char c = const () <$> satisfy (== c)
+char c = () <$ satisfy (== c) -- vagy void
+-- char c = void $ satisfy (== c) -- vagy ez
+
+-- Parseoljunk akármilyen karaktert
+anychar :: Parser Char
+anychar = satisfy (const True)
+
+-- Parseoljunk egy konkrét stringet
+-- hint: mapM_
+string :: String -> Parser ()
+-- string [] = return ()
+-- string (c:cs) = char c >> string cs
+string = traverse_ char --traverse ami nem kombinálja az eredményeket
+
+
+-- Parsernél fontos a "vagy" művelet (ha az első parser elhasal, akkor a másikat próbáljuk meg)
+-- Ez lesz az Alternative típusosztály
+-- hasonló az Applicative-hoz
+instance Alternative Parser where
+  empty :: Parser a -- Garantáltan elhasaló parser
+  empty = Parser $ const Nothing
+  (<|>) :: Parser a -> Parser a -> Parser a -- Ha a baloldali sikertelen, futassuk le a jobboldalit (hint: A Maybe is egy alternatív)
+  Parser l <|> Parser r = Parser $ \input -> case l input of
+    Just res -> Just res
+    Nothing -> r input
+
+-- Definiáljunk egy parsert ami egy 'a' vagy egy 'b' karaktert parseol
+
+aorb :: Parser Char
+aorb = satisfy (== 'a') <|> satisfy (== 'b')
+
+-- many :: Parser a -> Parser [a]
+-- 0 vagy többször lefuttatja a parsert
+-- some :: Parser a -> Parser [a]
+-- 1 vagy többször lefuttatja a parsert
+
+many' :: Parser a -> Parser [a]
+many' p = some' p <|> pure [] -- Ha 1 vagy több sikertelen, akkor 0 van
+
+some' :: Parser a -> Parser [a]
+some' p = (:) <$> p <*> many' p -- Lefuttatja 1x és utána 0 vagy többször
+
+-- optional' :: Parser a -> Parser (Maybe a)
+-- ha elhasalna a parser, mégse hasal el
+optional' :: Parser a -> Parser (Maybe a)
+optional' p = (Just <$> p) <|> return Nothing -- becsomagolja a Nothingot Justba
+
+-- replicateM :: Int -> Parser a -> Parser [a]
+-- n-szer lefuttat egy parsert
+replicateM' :: Integral i => i -> Parser a -> Parser [a]
+replicateM' i p
+  | i < 1 = return []
+  | otherwise = (:) <$> p <*> replicateM' (i-1) p
+
+
+-- asum :: [Parser a] -> Parser a
+-- Sorban megpróbálja az összes parsert lefuttatni
+asum' :: [Parser a] -> Parser a
+asum' [] = empty
+asum' (p:ps) = p <|> asum' ps
+
+-- Regex féle parserek
+{-
+    Regex gyorstalpaló:                               Haskell megfelelő:
+    c        - Parseol egy c karaktert                char 'c'
+    ℓ+       - Parseol 1 vagy több ℓ kifejezést       some ℓ
+    ℓ*       - Parseol 0 vagy több ℓ kifejezést       many ℓ
+    (ℓ₁|ℓ₂)  - Parseol ℓ₁-t vagy ℓ₂-t                 ℓ₁ <|> ℓ₂
+    ℓ?       - Parseol 0 vagy 1 ℓ kifejezést          optional ℓ
+    .        - Akármilyen karakter                    anyChar
+    ℓ{n}     - Parseol n darab ℓ kifejezést           replicateM n ℓ
+    ℓ{n,}    - Parseol legalább n darab ℓ kifejezést  replicateM n ℓ >> many ℓ
+    $        - Nincs mit parseolni                    eof
+    \d       - Parseol egy számjegyet                 digitToInt <$> satisfy isDigit
+    [c₁-c₂]  - c₁ és c₂ között parseol egy karaktert  satisfy (\x -> x >= min c₁ c₂ && x <= max c₁ c₂)
+    [c₁,c₂]  - c₁ vagy c₂ karaktert parseol           asum [char c₁, char c₂]
+-}
+
+-- Írjunk regex kifejezéseket parserekkel!
+
+-- alm(a|ák)
+p1 :: Parser ()
+p1 = 
+  string "alm" >>
+  char 'a' <|> string "ák"
+
+-- c(i+)ca
+p2 :: Parser ()
+p2 = 
+  char 'c' >>
+  some (char 'i') >>
+  string "ca"
+
+-- (c*)i?(c+)a{3}
+p3 :: Parser ()
+p3 = 
+  many (char 'c') >>
+  optional (char 'i') >>
+  some (char 'c') >>
+  -- (void $ replicateM 3 (char 'a'))
+  replicateM_ 3 (char 'a')
+
+-- (alma|banana)?
+p4 :: Parser ()
+p4 = void $ optional (string "alma" <|> string "banana") 
+
+-- [A-Z]{10}
+p5 :: Parser ()
+p5 = replicateM_ 10 (satisfy (\c -> c >= 'A' && c <= 'Z')) -- void $ _
+
+-- \d{2}
+p6 :: Parser ()
+p6 = replicateM_ 2 $ satisfy isDigit
+
+-- \d+.*$
+p7 :: Parser ()
+p7 = 
+  some (satisfy isDigit) >>
+  many anychar >>
+  eof
+
+-- \d?alma.?banana
+p8 :: Parser ()
+p8 = 
+  optional (satisfy isDigit) >>
+  string "alma" >>
+  optional anychar >>
+  string "banana"
+
+-- (ab)?ba+.*
+p9 :: Parser ()
+p9 = 
+  optional (string "ab") >>
+  char 'b' >>
+  some (char 'a') >>
+  () <$ many anychar
+
+-- \d{2,}-?$
+p10 :: Parser ()
+p10 = 
+  replicateM_ 2 (satisfy isDigit) >>
+  many (satisfy isDigit) >>
+  optional (char '-') >>
+  eof 
+
+-- [A-Z]*[1,2,3,9]?(A|Z)$
+p11 :: Parser ()
+p11 = undefined
+
+-- a+b*c?d{4}e{5,}
+p12 :: Parser ()
+p12 = undefined
+
+-- alm(a|ák)|banán(ok)?
+p13 :: Parser ()
+p13 = undefined
